37f1cb01362cd48292a0dfe519dc90175a5975c0
gxianlin <gxianlin@126.com>
2017-08-22 13:49:14 +0800

    <新增> 更改本地音乐页面样式

--- app/src/main/java/com/cainiao/music/cniaomusic/ui/radio/RadioFragment.java
+++ app/src/main/java/com/cainiao/music/cniaomusic/ui/radio/RadioFragment.java
@@ -1,5 +1,6 @@
 package com.cainiao.music.cniaomusic.ui.radio;
 
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.cainiao.music.cniaomusic.R;
@@ -7,6 +8,7 @@ import com.cainiao.music.cniaomusic.ui.base.BaseFragment;
 import com.cainiao.music.cniaomusic.ui.cnmusic.LocalMusicActivity;
 import com.cainiao.music.cniaomusic.ui.cnmusic.RecentPlayListActivity;
 
+import butterknife.InjectView;
 import butterknife.OnClick;
 
 /**
@@ -18,6 +20,9 @@ import butterknife.OnClick;
  * 邮箱：gxianlin@126.com
  */
 public class RadioFragment extends BaseFragment {
+
+    @InjectView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
 
     public RadioFragment() {
         // Required empty public constructor
@@ -44,14 +49,24 @@ public class RadioFragment extends BaseFragment {
 
     }
 
-    @OnClick({R.id.local_music, R.id.recent_play})
+    @Override
     public void onClick(View view) {
+
+    }
+
+
+    @OnClick({R.id.local_layout, R.id.recently_layout, R.id.download_layout, R.id.artist_layout})
+    public void onViewClicked(View view) {
         switch (view.getId()) {
-            case R.id.local_music:
+            case R.id.local_layout:
                 startToActivity(LocalMusicActivity.class);
                 break;
-            case R.id.recent_play:
+            case R.id.recently_layout:
                 startToActivity(RecentPlayListActivity.class);
+                break;
+            case R.id.download_layout:
+                break;
+            case R.id.artist_layout:
                 break;
         }
     }
